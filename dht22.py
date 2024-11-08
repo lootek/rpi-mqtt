1e818a67ffc0de584f459064ee910063cd6f4971
Piotr Lucinski <lootek87@gmail.com>
2024-11-08 18:13:01 +0100

    Add dht22, cleanup, refactor volumes

--- dht22.py
+++ dht22.py
@@ -39,10 +39,10 @@ def publish(client, topic, msg):
 
 def measure():
     while True:
-        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)
+        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
         print("Temp: {0:0.1f} C  Humidity: {1:0.1f} %".format(temperature, humidity))
-        publish(client, "/sensors/attic/dht11/temperature", temperature)
-        publish(client, "/sensors/attic/dht11/humidity", humidity)
+        publish(client, "/sensors/attic/dht22/temperature", temperature)
+        publish(client, "/sensors/attic/dht22/humidity", humidity)
 
         sleep(60)
 
